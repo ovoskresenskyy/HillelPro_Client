7bab1c5b9912b2fbb42e08613d35a239bde1edca
ovoskresenskiy <7949894@ukr.net>
2022-12-01 17:31:00 +0200

    Rename to MyClient for avoid naming conflicts
    Add file sender (testing)

--- src/main/java/org/example/model/MyClient.java
+++ src/main/java/org/example/model/MyClient.java
@@ -1,21 +1,37 @@
-package org.example;
+package org.example.model;
+
+import org.example.service.FileSender;
+import org.example.service.ServerReader;
+import org.example.service.ServerWriter;
 
 import java.io.*;
 import java.net.Socket;
 
-public class Client {
+public class MyClient {
 
-    public static void main(String[] args) {
+    private MyClient(){}
+
+    private static class ClientHolder {
+        private final static MyClient instance = new MyClient();
+    }
+
+    public static MyClient getInstance() {
+        return MyClient.ClientHolder.instance;
+    }
+
+
+    public void start() {
         try {
             Socket clientSocket = new Socket("localhost", 10160);
             BufferedReader listener = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             BufferedWriter sender = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
             BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+            FileSender fileSender = new FileSender(clientSocket);
 
             ServerReader serverReader = new ServerReader(listener);
             serverReader.start();
 
-            ServerWriter serverWriter = new ServerWriter(userInputReader, sender);
+            ServerWriter serverWriter = new ServerWriter(userInputReader, sender, fileSender);
             serverWriter.start();
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -23,7 +39,7 @@ public class Client {
         }
     }
 
-    public static void closeConnection(){
+    public void closeConnection(){
         System.out.println("You was disconnected from the server. Come back later.");
         System.exit(0);
     }
